88c4d75a7b06039d12b846c9a72cd7f4b670253d
Sahej17 <43532447+Sahej17@users.noreply.github.com>
2019-09-21 09:18:14 +0530

    Update initial.py

--- initial.py
+++ initial.py
@@ -4,6 +4,7 @@ import playsound
 import speech_recognition as sr
 from gtts import gTTS
 
+
 def speak(text):
     tts = gTTS(text=text, lang="en")
     filename = "voice.mp3"
@@ -11,4 +12,23 @@ def speak(text):
     playsound.playsound(filename)
 
 
-speak("hello sahej")
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said
+
+text = get_audio()
+
+if "hello" in text:
+    speak("hello, how are you?")
+elif "what is your name" in text:
+    speak("My name is Sahej")
